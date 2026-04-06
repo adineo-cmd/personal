7e46e2b575da2c920b652dbe6b88ef45fedef08f
Aditya Yadav <141362606+adineo-cmd@users.noreply.github.com>
2026-04-06 12:42:21 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+const isGitHub = process.env.GITHUB_PAGES === 'true';
+
 export default defineConfig({
-  site: 'https://adineo.neocities.org',
+  site: isGitHub ? 'https://adineo-cmd.github.io/personal' : 'https://adineo.neocities.org',
+  base: isGitHub ? '/personal' : undefined,
   trailingSlash: 'always',
   integrations: [sitemap()],
   output: 'static',
